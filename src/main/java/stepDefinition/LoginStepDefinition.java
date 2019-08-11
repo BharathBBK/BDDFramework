e1ff59fcf17cb842ee4bbdc3643a19c2be19b62e
Bharath <Bharathkumar0732@gmail.com>
2019-08-11 17:25:23 +0530

    Hook is added along with tags

--- src/main/java/stepDefinition/LoginStepDefinition.java
+++ src/main/java/stepDefinition/LoginStepDefinition.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -14,19 +16,35 @@ import junit.framework.Assert;
 public class LoginStepDefinition {
 	
 	WebDriver driver;
+	@Before
+	public void beforeM() {
+		
+		System.out.println("Hooks concept before method");
+	}
+	
+	@After
+   public void AfterM() {
+		
+		System.out.println("Hooks concept After method");
+	}
+	
+	
+	
 	
 	@Given ("^user is already on Login Page$")
 	public void user_already_on_login_page() {
-		System.setProperty("webdriver.chrome.driver", "E:\\Automation\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.get("https://www.freecrm.com/index.html");
+//		System.setProperty("webdriver.chrome.driver", "E:\\Automation\\chromedriver.exe");
+//		driver = new ChromeDriver();
+//		driver.get("https://www.freecrm.com/index.html");
+		System.out.println("User login");
 				
 	}
 	 @When("^title of login page is Free CRM$")
 	 public void title_of_login_page_is_free_CRM(){
-	 String title = driver.getTitle();
-	 System.out.println(title);
-	 Assert.assertEquals("Free CRM software for any Business", title);
+//	 String title = driver.getTitle();
+//	 System.out.println(title);
+//	 Assert.assertEquals("Free CRM software for any Business", title);
+		 System.out.println("title validation");
 	 }
 	
 	 //Reg Exp:
@@ -34,26 +52,54 @@ public class LoginStepDefinition {
 	 //2. \"(.*)\"
 	
 	 @Then("^user enters username and password$")
-	 public void user_enters_username_and_password(String username, String password){
-	 driver.findElement(By.name("username")).sendKeys("naveenk");
-	 driver.findElement(By.name("password")).sendKeys("test@123");
+	 public void user_enters_username_and_password(){
+//	 driver.findElement(By.name("username")).sendKeys("naveenk");
+//	 driver.findElement(By.name("password")).sendKeys("test@123");
+		 System.out.println("valid username and password");
 	 }
 	
 	 @Then("^user clicks on login button$")
 	 public void user_clicks_on_login_button() {
-	 WebElement loginBtn =
-	 driver.findElement(By.xpath("//input[@type='submit']"));
-	 JavascriptExecutor js = (JavascriptExecutor)driver;
-	 js.executeScript("arguments[0].click();", loginBtn);
+//	 WebElement loginBtn =
+//	 driver.findElement(By.xpath("//input[@type='submit']"));
+//	 JavascriptExecutor js = (JavascriptExecutor)driver;
+//	 js.executeScript("arguments[0].click();", loginBtn);
+		 System.out.println("user click on login");
 	 }
 	
 	
 	 @Then("^user is on home page$")
 	 public void user_is_on_hopme_page(){
-	 String title = driver.getTitle();
-	 System.out.println("Home Page title ::"+ title);
-	 Assert.assertEquals("CRMPRO", title);
+//	 String title = driver.getTitle();
+//	 System.out.println("Home Page title ::"+ title);
+//	 Assert.assertEquals("CRMPRO", title);
+		 System.out.println("user is on home page");
 	 }
+	 
+	 @Given("^user is on Home Page$")
+	 public void user_is_on_Home_Page() throws Throwable {
+	     // Write code here that turns the phrase above into concrete actions
+	    System.out.println("User in Home page");
+	 }
+
+	 @When("^user is selected contacts$")
+	 public void user_is_selected_contacts() throws Throwable {
+	     // Write code here that turns the phrase above into concrete actions
+	  System.out.println("User is selected contacts");
+	 }
+
+	 @Then("^user created  contact$")
+	 public void user_created_contact() throws Throwable {
+	     // Write code here that turns the phrase above into concrete actions
+	  System.out.println("user created contact");
+	 }
+
+	 @Then("^user click on logout$")
+	 public void user_click_on_logout() throws Throwable {
+	     // Write code here that turns the phrase above into concrete actions
+     System.out.println("user click on logout");
+	 }
+
 	
 	
 
--- test-outout/report.js
+++ test-outout/report.js
@@ -6,36 +6,46 @@ formatter.feature({
   "id": "free-crm-login-page-feature",
   "keyword": "Feature"
 });
+formatter.before({
+  "duration": 459139,
+  "status": "passed"
+});
 formatter.scenario({
-  "line": 3,
+  "line": 4,
   "name": "Free CRM Login Test Scenario",
   "description": "",
   "id": "free-crm-login-page-feature;free-crm-login-test-scenario",
   "type": "scenario",
-  "keyword": "Scenario"
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 3,
+      "name": "@SmokeTest"
+    }
+  ]
 });
 formatter.step({
-  "line": 5,
+  "line": 6,
   "name": "user is already on Login Page",
   "keyword": "Given "
 });
 formatter.step({
-  "line": 6,
+  "line": 7,
   "name": "title of login page is Free CRM",
   "keyword": "When "
 });
 formatter.step({
-  "line": 7,
+  "line": 8,
   "name": "user enters username and password",
   "keyword": "Then "
 });
 formatter.step({
-  "line": 8,
+  "line": 9,
   "name": "user clicks on login button",
   "keyword": "Then "
 });
 formatter.step({
-  "line": 9,
+  "line": 10,
   "name": "user is on home page",
   "keyword": "Then "
 });
@@ -43,33 +53,109 @@ formatter.match({
   "location": "LoginStepDefinition.user_already_on_login_page()"
 });
 formatter.result({
-  "duration": 33403671449,
+  "duration": 137723924,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginStepDefinition.title_of_login_page_is_free_CRM()"
 });
 formatter.result({
-  "duration": 151399165,
-  "error_message": "junit.framework.ComparisonFailure: expected:\u003c[#1 Free CRM for Any Business: Online Customer Relationship Software]\u003e but was:\u003c[Free CRM software for any Business]\u003e\r\n\tat junit.framework.Assert.assertEquals(Assert.java:100)\r\n\tat junit.framework.Assert.assertEquals(Assert.java:107)\r\n\tat stepDefinition.LoginStepDefinition.title_of_login_page_is_free_CRM(LoginStepDefinition.java:29)\r\n\tat ✽.When title of login page is Free CRM(C:/Users/User/eclipse-workspace/BDDFrame/src/main/java/features/login.Feature:6)\r\n",
-  "status": "failed"
+  "duration": 148767,
+  "status": "passed"
 });
 formatter.match({
-  "location": "LoginStepDefinition.user_enters_username_and_password(String,String)"
+  "location": "LoginStepDefinition.user_enters_username_and_password()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 202384,
+  "status": "passed"
 });
 formatter.match({
   "location": "LoginStepDefinition.user_clicks_on_login_button()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 159340,
+  "status": "passed"
 });
 formatter.match({
   "location": "LoginStepDefinition.user_is_on_hopme_page()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 114407,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 156318,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 1022867,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 14,
+  "name": "Free CRM Home Page",
+  "description": "",
+  "id": "free-crm-login-page-feature;free-crm-home-page",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 13,
+      "name": "@RegressionTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 16,
+  "name": "user is on Home Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 17,
+  "name": "user is selected contacts",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 18,
+  "name": "user created  contact",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 19,
+  "name": "user click on logout",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_is_on_Home_Page()"
+});
+formatter.result({
+  "duration": 195964,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_is_selected_contacts()"
+});
+formatter.result({
+  "duration": 72495,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_created_contact()"
+});
+formatter.result({
+  "duration": 134041,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_click_on_logout()"
+});
+formatter.result({
+  "duration": 175198,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 55505,
+  "status": "passed"
 });
 });
